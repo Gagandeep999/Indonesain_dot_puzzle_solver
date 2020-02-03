5a68bef1eeee8c238989ca327ace661f7753ac89
Tarek Deghayli <tarekmdeghayli@gmail.com>
2020-02-03 13:04:18 -0500

    max depth and max node features have been implemented. (further testing to be done)

--- Puzzle_Solver.py
+++ Puzzle_Solver.py
@@ -9,16 +9,16 @@ Starting point of the solver
 
 def main():
     # input_file_path = input("Enter input file path: ")
-    input_file_path = 'sample/test.txt'
+    input_file_path = 'sample_1/test_1.txt'
     args = open(input_file_path)
     for lines in args:
-        size, max_d, _, initial = lines.split(" ")
+        size, max_d, max_nodes, initial = lines.split(" ")
         initial = initial.rstrip()
         board = Board(int(size))
         board.initialize_board(initial)
         # board.print_board
 
-        game = Play_Game(board, max_d)
+        game = Play_Game(board, max_d,max_nodes)
         game.play_game()
 
 
--- play_game.py
+++ play_game.py
@@ -3,21 +3,21 @@ import copy
 
 class Play_Game():
 
-    def __init__(self, board, max_d):
+    def __init__(self, board, max_d, max_node):
         self.board = board
         self.stack = [self.board]
         self.parentMap = {}
         self.visits = []
-        self.max_d = max_d
-        self.current_depth = 1
-
+        self.max_d = int(max_d)
+        self.max_node = int(max_node)
+        self.depth = {}
     '''
     Takes a position and the board and depending on the position flips surrounding position and returns new board
     '''
 
     def flip_board(self, i, board):
         size = board.size
-        kid = copy.deepcopy(board)
+        parent = copy.deepcopy(board)
         board_1 = copy.deepcopy(board)
         if i % size == 0:
             board_1.flip(i - size)
@@ -36,7 +36,7 @@ class Play_Game():
             board_1.flip(i + 1)
             board_1.flip(i + size)
         if str(board_1.current_state) not in self.parentMap:
-            self.parentMap[str(board_1.current_state)] = str(kid.current_state)
+            self.parentMap[str(board_1.current_state)] = str(parent.current_state)
         return board_1
 
     '''
@@ -56,10 +56,12 @@ class Play_Game():
     '''
 
     def generate_children(self, board):
-        for i in range(board.size+1, -1, -1):
+        for i in range(board.size*board.size-1, -1, -1):
             kid = self.flip_board(i, board)
-            if str(kid.current_state) not in self.visits and str(kid.current_state) not in self.stack:
+            if str(kid.current_state) not in self.visits:
                 self.stack.append(kid)
+                # gets the parents depth and adds to that
+                self.depth[str(kid.current_state)] = self.depth.get((self.parentMap.get(str(kid.current_state)))) + 1
         pass
 
     '''
@@ -71,24 +73,33 @@ class Play_Game():
 
     def play_game(self):
         self.parentMap[str(self.board.current_state)] = "root"
+        self.depth[str(self.board.current_state)] = 0
         while self.stack.__len__() != 0:
             new_board = self.stack.pop()
-            self.visits.append(str(new_board.current_state))
             if self.is_final_state(new_board):
-                print('success')
-                state = str([0 for i in range(self.board.size*self.board.size)])
-                # print(parentMap)
-                print(state)
-                while self.parentMap.get(state) != "root":
-                    print(self.parentMap.get(state))
-                    state = self.parentMap.get(state)
-                # method for printing all the appropriate shit
+                self.generate_report()
                 return
-            else:
-                # self.current_depth += 1
-                self.generate_children(new_board)
-
-                # to keep track of when we come back out
-                # self.current_depth -= 1
+            # if we still can go deeper into the tree
+            if self.max_d > self.depth.get(str(new_board.current_state)):
+                # have we reached the max node count.
+                if self.max_node > self.visits.__len__() and self.max_d > 0:
+                    # node hasnt been visited yet
+                    if str(new_board.current_state) not in self.stack:
+                        self.visits.append(str(new_board.current_state))
+                        self.generate_children(new_board)
+                else:
+                    # generate_failed_report()
+                    self.stack.clear()
         print("no solution exists")
         return
+
+
+    def generate_report(self):
+        print('success')
+        state = str([0 for i in range(self.board.size * self.board.size)])
+        # print(parentMap)
+        print(state)
+        while self.parentMap.get(state) != "root":
+            print(self.parentMap.get(state))
+            state = self.parentMap.get(state)
+        # method for printing all the appropriate shit
